909da95df1df2a0776118ae63e43a40a5f922f7d
DagemBefikadu <befikadu.dagem@gmail.com>
2022-01-11 13:16:36 -0800

    Detail page and comments render

--- src/components/campaign/CampaignDetail.js
+++ src/components/campaign/CampaignDetail.js
@@ -2,15 +2,23 @@ import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import apiUrl from '../../apiConfig'
+import Comment from "./Comment";
 
-export default function CampaignDetail() {
+export default function CampaignDetail(props) {
     
     const[detailedCampaign, setDetailedCampaign] = useState([])
 
+    const [allComment, setAllComment] = useState([]);
+
     useEffect(() => {
-        console.log('getting item')
+        // console.log('getting item')
         getCampaign()
       }, [])
+
+    useEffect(() => {
+        getComment();
+        console.log("all Comment: ", allComment);
+      }, []);
 
     let newParam = useParams()  
 
@@ -19,10 +27,33 @@ export default function CampaignDetail() {
         .then(response=>response.json())
         .then(foundCampaign => {
             setDetailedCampaign(foundCampaign.campaign)
-            console.log('singleItem ', foundCampaign.campaign)
+            // console.log('singleItem ', foundCampaign.campaign)
         })
 
     }
+
+    const getComment = () => {
+        fetch(apiUrl + `/campaigns/${newParam.id}/comments`)
+            .then((response) => response.json())
+            .then((foundComments) => {
+            setAllComment(foundComments.comments);
+            console.log(foundComments)
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+    }
+
+    let commentList = allComment.map((c) => {
+        console.log(c);
+        return (
+          <Comment
+            comment={c}
+            user={props.user}
+            key={c._id}
+          />
+        );
+      });
     
     return (
         <div>
@@ -30,6 +61,8 @@ export default function CampaignDetail() {
             <h1>{detailedCampaign.name}</h1>
             <p>{detailedCampaign.cause}</p>
             <small>{detailedCampaign.location}</small>
+            <hr />
+            {commentList}
         </div>
     )
 }
--- src/components/campaign/Comment.js
+++ src/components/campaign/Comment.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function Comment() {
+
+export default function Comment(props) {
+
+    if (props.user && props.comment.owner === props.user._id){
+        return (
+            <>
+               <div ><p>{props.comment.commented}</p></div>
+               <small>{props.comment.owner.name} </small>
+            </>
+       )
+        } else {        
     return (
         <div>
-            
+            <p>{props.comment.commented}</p>
+            <small>-{props.comment.owner.name}</small>
         </div>
-    )
+    )}
 }
